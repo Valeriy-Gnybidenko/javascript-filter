a0e8a7c2dbe448f14f9104469bf44c7b5965f95d
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-21 13:27:38 +0200

    feature: add javascript code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import './template.html';
 import './assets/styles/index.scss';
+
+const btns = document.querySelectorAll('.filter__btn');
+const filterProducts = document.querySelectorAll('.filter__product');
+
+const onClick = (e) => {
+  let targetData = e.currentTarget.dataset.filter;
+
+  filterProducts.forEach((el) => {
+    if (targetData === el.dataset.filter) {
+      el.classList.add('show');
+      el.classList.remove('hide');
+    } else if (targetData === 'all') {
+      el.classList.add('show');
+      el.classList.remove('hide');
+    } else {
+      el.classList.add('hide');
+      el.classList.remove('show');
+    }
+  });
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', onClick);
+});
